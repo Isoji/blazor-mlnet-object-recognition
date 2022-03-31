a8d1ae30ae24e29cc7bb1a081c1fec559f5022ec
Isoji <tremblayben@hotmail.ca>
2022-04-01 00:24:56 +0100

    Add PreProcessImage method

--- ObjectRecogntionWebApp/Model/FasterRCNN.cs
+++ ObjectRecogntionWebApp/Model/FasterRCNN.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.Fonts;
 
 namespace ObjectRecogntionWebApp.Model
 {
@@ -12,7 +18,37 @@ namespace ObjectRecogntionWebApp.Model
         }
         public void DetectObjects(string imageUrl, HashSet<string> labels, float confidence)
         {
-            throw new NotImplementedException();
+            Image image = Image<Bgr24>.Load(imageUrl);
+        }
+
+        public Tensor<float> PreProcessImage(Image<Bgr24> image)
+        {
+            var processedImage = image.Clone();
+            // Resize image
+            float ratio = 800f / Math.Min(processedImage.Width, processedImage.Height);
+            processedImage.Mutate(x => x.Resize((int)(ratio * processedImage.Width), (int)(ratio * processedImage.Height)));
+
+            // Pad to be divisiable by 32
+            var paddedHeight = (int)(Math.Ceiling(processedImage.Height / 32f) * 32f);
+            var paddedWidth = (int)(Math.Ceiling(processedImage.Width / 32f) * 32f);
+
+            var input = new DenseTensor<float>(new[] { 3, paddedHeight, paddedWidth });
+
+            // Normalize image
+            var mean = new[] { 102.9801f, 115.9465f, 122.7717f };
+            processedImage.ProcessPixelRows(accessor => {
+                for (int y = paddedHeight - processedImage.Height; y < processedImage.Height; y++)
+                {
+                    var pixelRow = accessor.GetRowSpan(y);
+                    for (int x = paddedWidth - processedImage.Width; x < processedImage.Width; x++)
+                    {
+                        input[0, y, x] = pixelRow[x].B - mean[0];
+                        input[1, y, x] = pixelRow[x].G - mean[1];
+                        input[2, y, x] = pixelRow[x].R - mean[2];
+                    }
+                }
+            });
+            return input;
         }
     }
 
