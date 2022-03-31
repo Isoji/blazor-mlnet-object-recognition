2c33cc12e3a30548b0c17fdd378a14a2be3b832b
Isoji <tremblayben@hotmail.ca>
2022-03-31 19:11:16 +0100

    Add Box class

--- ObjectRecogntionWebApp/Model/FasterRCNN.cs
+++ ObjectRecogntionWebApp/Model/FasterRCNN.cs
@@ -15,4 +15,24 @@ namespace ObjectRecogntionWebApp.Model
             throw new NotImplementedException();
         }
     }
+
+    /// <summary>
+    /// Stores the coordinates for a bounding box
+    /// </summary>
+    public class Box
+    {
+        public float Xmin { get; set; }
+        public float Ymin { get; set; }
+        public float Xmax { get; set; }
+        public float Ymax { get; set; }
+
+        public Box(float xmin, float ymin, float xmax, float ymax)
+        {
+            Xmin = xmin;
+            Ymin = ymin;
+            Xmax = xmax;
+            Ymax = ymax;
+
+        }
+    }
 }
