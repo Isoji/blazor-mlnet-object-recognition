1c5d66e0e9c255f7a65b7999cf876249d8ae2b2e
Isoji <tremblayben@hotmail.ca>
2022-03-31 19:03:57 +0100

    Add FasterRCNN class

---
+++ ObjectRecogntionWebApp/Model/FasterRCNN.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectRecogntionWebApp.Model
+{
+    public class FasterRCNN : IDetector
+    {
+        public void DetectObjects(string imageUrl, HashSet<string> labels, float confidence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
