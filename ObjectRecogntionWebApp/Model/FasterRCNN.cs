72608ffa56124da3c960569a7452b17cc55b8ba1
Isoji <tremblayben@hotmail.ca>
2022-03-31 19:09:15 +0100

    Add class constructor

--- ObjectRecogntionWebApp/Model/FasterRCNN.cs
+++ ObjectRecogntionWebApp/Model/FasterRCNN.cs
@@ -1,7 +1,15 @@
-﻿namespace ObjectRecogntionWebApp.Model
+﻿using Microsoft.ML.OnnxRuntime;
+
+namespace ObjectRecogntionWebApp.Model
 {
     public class FasterRCNN : IDetector
     {
+        readonly InferenceSession Session;
+
+        public FasterRCNN()
+        {
+            Session = new InferenceSession("/wwwroot/onnx/FasterRCNN-10.onnx");
+        }
         public void DetectObjects(string imageUrl, HashSet<string> labels, float confidence)
         {
             throw new NotImplementedException();
