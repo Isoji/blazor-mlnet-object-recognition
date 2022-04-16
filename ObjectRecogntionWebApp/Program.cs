81f9d7b94ac9508347dccc403abae0e7a1134359
Isoji <tremblayben@hotmail.ca>
2022-04-16 17:30:42 +0100

    Remove Syncfusion import and services

--- ObjectRecogntionWebApp/Program.cs
+++ ObjectRecogntionWebApp/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 
 var app = builder.Build();
 
